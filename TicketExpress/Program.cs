7a48ae470767232c14b188fe540bb2a97d0e1cd7
JP-019 <juliocgp30@gmail.com>
2026-09-22 20:07:21 -0600

    Plantilla de proyecto con Instrucciones en README.mb

---
+++ TicketExpress/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TicketExpress.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TicketExpressContext>(options =>
+    options.UseInMemoryDatabase("TicketExpressDB"));
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
